e681b7fc69a4795634148171e3ae9376080ecc23
Sachin M K <sachinmk27@gmail.com>
2025-01-26 17:44:21 +0530

    Add load test script

--- src/load-test.js
+++ src/load-test.js
@@ -6,9 +6,20 @@ export let errorRate = new Rate("errors");
 
 export let options = {
   stages: [
-    { duration: "10s", target: 10 },
-    { duration: "10s", target: 10 },
-    { duration: "10s", target: 0 },
+    { duration: "10s", target: 200 },
+    { duration: "10s", target: 400 },
+    { duration: "10s", target: 600 },
+    { duration: "10s", target: 800 },
+    { duration: "10s", target: 1000 },
+    { duration: "10s", target: 2000 },
+    { duration: "10s", target: 3000 },
+    { duration: "10s", target: 4000 },
+    { duration: "10s", target: 5000 },
+    { duration: "10s", target: 6000 },
+    { duration: "10s", target: 7000 },
+    { duration: "10s", target: 8000 },
+    { duration: "10s", target: 9000 },
+    { duration: "10s", target: 10000 },
   ],
   summaryTrendStats: [
     "avg",
